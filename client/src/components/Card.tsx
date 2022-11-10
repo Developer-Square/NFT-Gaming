814b5dbff20741a2008213b5b6f4ec6d4c2c3433
ryann254 <keddelyronjoz@gmail.com>
2022-11-10 12:05:59 +0300

    feat: add details to the card component

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
+
+import styles from '../styles';
+import { allCards } from '../assets';
 import { IPlayer } from '../page/Battle';
 
 type Props = {
@@ -9,8 +13,37 @@ type Props = {
   playerTwo?: boolean;
 };
 
-const Card = (props: Props) => {
-  return <div>Card</div>;
+const generateRandomCardImage = () =>
+  allCards[Math.floor(Math.random() * (allCards.length - 1))];
+
+const img1 = generateRandomCardImage();
+const img2 = generateRandomCardImage();
+
+const Card = ({ card, title, cardRef, restStyles, playerTwo }: Props) => {
+  return (
+    <div className={`${styles.cardContainer} ${restStyles}`}>
+      <img
+        src={playerTwo ? img2 : img1}
+        alt='card'
+        className={styles.cardImg}
+      />
+
+      <div
+        className={`${styles.cardPointContainer} sm:left-[27.2%] left-[29%] ${styles.flexCenter}`}
+      >
+        <p className={`${styles.cardPoint} text-yellow-400`}>{card?.att}</p>
+      </div>
+      <div
+        className={`${styles.cardPointContainer} sm:right-[22.5%] right-[25%] ${styles.flexCenter}`}
+      >
+        <p className={`${styles.cardPoint} text-red-700`}>{card?.def}</p>
+      </div>
+
+      <div className={`${styles.cardTextContainer} ${styles.flexCenter}`}>
+        <p className={styles.cardText}>{title}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
